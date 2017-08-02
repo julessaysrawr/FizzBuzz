008d489c218bad44b01e53a93278eb506bd869c2
Julie Chevalier <julessaysrawr@users.noreply.github.com>
2017-08-01 20:17:27 -0400

    simplifying code
    removes duplicate code
    this also allows for future additions like if % 7 should be derp

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,16 @@
-var output = [];
-
 function fizzBuzz(number) {
   for (i = 1; i <= number; i++) {
-      if ((i % 3 === 0) && (i % 5 === 0)) {
-          console.log("FizzBuzz");
-          output.push("FizzBuzz");
-      } else if (i % 3 === 0) {
-          console.log("Fizz");
-          output.push("Fizz");
-      } else if (i % 5 === 0) {
-          console.log("Buzz");
-          output.push("Buzz");
-      } else {
-          console.log(i);
-          output.push(i);
+
+    var output = "";
+
+      if (i % 3 === 0) { output += "Fizz"; }
+      if (i % 5 === 0) { output += "Buzz"; }
+
+      if (output === "") { output = i; }
+
+          console.log(output);
       }
-  };
+
 }
 var number = prompt("How far to play FizzBuzz up to?");
 
